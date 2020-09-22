fa4adaecdd181499d7fef7bf601c3d073ec084c1
EisenritterX <georgezhou89@hotmail.com>
2020-09-22 11:14:53 -0400

    Initial commit

---
+++ Week2Code/__init__.py
@@ -0,0 +1,30 @@
+'''
+Written by George Zhou
+September 22, 2020
+
+Demonstrate how to wriite a simple program and
+make iit avalable to other program
+'''
+
+print("Hello World")
+a = 'hello world'
+a = 8 + 9
+print(a)
+type(a)
+dir()
+#List all Python Packages
+#python -m pydoc -b
+#Using argparse package
+
+PI = 3.1415926
+    def add(x,y):
+        return x+y
+
+    def diff(x,y):
+        return  x-y
+
+    def area_of_circle(r):
+        return PI * r ** 2
+    def area_of_rectangle(w,length):
+        return w*length
+    
